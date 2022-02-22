3b849c736a70717bbfd22d2bffa5586fa3d55100
Jiayu Zhang <jiayuzhangg@gmail.com>
2022-02-22 01:31:11 -0500

    can't get post to work

---
+++ server.js
@@ -0,0 +1,63 @@
+const Sequelize = require('sequelize')
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/dealers_choice_spa_db')
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const Food = db.define('food',{
+    name:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate:{
+            notEmpty:true
+        }
+    }
+})
+
+app.use('/src', express.static(path.join(__dirname, 'src')))
+
+app.get('/', (req, res)=>res.sendFile(path.join(__dirname, 'index.html')))
+
+app.delete('/api/foods/:id', async(req, res, next)=>{
+    try{
+        const food = await Food.findByPk(req.params.id)
+        await food.destroy()
+        res.sendStatus(204)
+    }catch(ex){
+        next(ex)
+    }
+})
+
+app.post('/api/foods', async(req, res, next)=>{
+    try{
+        console.log(`teehee ${req.body}`)
+        res.send(req.body)
+        // await Food.create({name: req.body})
+    }catch(ex){
+        next(ex)
+    }
+})
+
+app.get('/api/foods',async(req, res, next)=>{
+    try{
+        res.send(await Food.findAll())
+    }catch(ex){
+        next(ex)
+    }
+})
+
+const setUp = async(req, res, next)=>{
+    try{
+        await Food.sync({force:true})
+        await Food.create({name:'salad'})
+        await Food.create({name:'steak'})
+        await Food.create({name:'noodles'})
+        await Food.create({name:'hamburger'})
+        const port = process.env.PORT || 3000
+        app.listen(port, ()=>console.log(`listening on port ${port}`))
+    }catch(ex){
+        console.log(ex)
+    }
+}
+
+setUp()
---
+++ src/index.js
@@ -0,0 +1,36 @@
+const ul = document.querySelector('ul')
+
+ul.addEventListener('click', async(ev)=>{
+    if(ev.target.tagName === 'LI'){
+        const id = ev.target.getAttribute('data-id')
+        await axios.delete(`/api/foods/${id}`)
+        init()
+    }
+})
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', async(ev)=>{
+    ev.preventDefault()
+    const newFood = document.getElementById('input_item').value
+    console.log(newFood)
+    await axios
+        .post('/api/foods', {"name":newFood})
+        .then(res=> console.log(res))
+        .catch(err=>console.log(err))
+    init()
+})
+
+const init = async()=>{
+    const response = await axios.get('/api/foods')
+    const foods = response.data
+    const html = foods.map(food=>{
+        return `
+            <li data-id='${food.id}'>${food.name}</li>
+        `
+    }).join('')
+
+    ul.innerHTML = html
+}
+
+init()
